d0f3b4050bfaf137b4aebdd00f22d3768921dd55
Julius Rye <juliusrye@gmail.com>
2022-12-05 07:59:12 +0100

    added fit to ball data

--- error_propagation_ball_Julius.py
+++ error_propagation_ball_Julius.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+from iminuit import Minuit
 
 def get_peak(data: pd.DataFrame) -> tuple[pd.DataFrame, list[float]]:
     t = data.loc[data['signal'].argmax(), 'time']
@@ -22,7 +23,12 @@ def get_peaks(data: pd.DataFrame) -> list[float]:
     peaks.sort()
     return peaks
 
-norm_dir = pd.read_csv('Lab/testdata/data_NormDir_MedBall1.txt', delimiter='\t', header=None).rename(columns={0: 'time', 1: 'signal'})
+norm_dir = pd.read_csv('Ball/Data/forsøg1_bigball_left.csv', delimiter=',', header=13).rename(columns={'Time (s)': 'time', 'Channel 1 (V)': 'signal'})
+peaks = get_peaks(norm_dir)
+d = 0.05
+p = np.linspace(0, 1.9, 5)#[.5, .75, 1, 1.25, 1.5]
+pos = np.kron(p, [1,1])+np.kron([1,1,1,1,1], [-d/2, d/2])
+
 data = pd.DataFrame(data={
     'a': [1],
     'ua': [.1],
@@ -34,9 +40,29 @@ data = pd.DataFrame(data={
     'ud': [.1],
 })
 
+x = np.array(peaks)
+y = np.array(pos)
+ey = 0.0025
+def fit(x, a, b, c):
+    return a*x**2 + b*x + c
 
-peaks = get_peaks(norm_dir)
-print(peaks)
+def chi2(a, b, c):
+    y_fit = fit(x, a, b, c)
+    return np.sum(((y - y_fit) / ey)**2)
+chi2.errordef = 1.0
+
+minuit = Minuit(chi2, a=1, b=0, c=0)
+minuit.migrad()
+print(minuit.values)
+print(minuit.errors)
+print(f'reduced chi2: {minuit.fval / 7 :.2f}')
+
+xx = np.linspace(x[0], x[-1], 1000)
+yy = fit(xx, *minuit.values)
+plt.figure()
+plt.plot(xx, yy)
+plt.plot(x, y, '.')
+plt.show()
 
 data['g'] = data['a']/np.sin(data['theta'])*(1+2/5*data['D']**2/(data['D']**2-data['d']**2))
 data['ug_a'] = np.abs((1+2*data['D']**2/5/(data['D']**2-data['d']**2))/np.sin(data['theta'])*data['ua'])
